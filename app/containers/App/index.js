6b45d6b3db8b097143e9df5b743be3da18cba2b6
Erfan Akhavan <erfan@Erfans-MacBook-Pro.local>
2019-09-06 17:26:24 +0430

    Fix Some Stuff

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,20 +9,26 @@
 
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
 
 import HomePage from 'containers/HomePage/Loadable';
+import Authenication from 'containers/Authenication';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-
+import theme from '../../theme';
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={Authenication} />
+          <Route exact path="/register" component={Authenication} />
+          <Route component={NotFoundPage} />
+        </Switch>
+        <GlobalStyle />
+      </ThemeProvider>
     </div>
   );
 }
